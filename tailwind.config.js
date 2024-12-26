2096633b8d68f15db12dc0948c8bbdade8dc8ebd
Callum Adair <callum.adair@outlook.com>
2024-12-26 18:05:53 +0000

    better trunk and tailwindcss configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./index.html", "./src/*.rs"],
+    content: ["./index.html", "./src/**/*.rs"],
     theme: {
         extend: {},
     },
