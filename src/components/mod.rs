689e1b963edb125d05aa434bc7e8c98e849bd399
Callum Adair <callum.adair@outlook.com>
2025-01-01 22:08:21 +0000

    updated cli software.rs

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,1 +1,2 @@
 pub mod carousel;
+pub mod table;
--- src/pages/software.rs
+++ src/pages/software.rs
@@ -1,4 +1,8 @@
+use std::rc::Rc;
+
 use yew::prelude::*;
+
+use crate::components::table::Table;
 
 #[function_component(SoftwareBase)]
 pub fn base() -> Html
@@ -13,7 +17,13 @@ pub fn base() -> Html
 #[function_component(CLIBase)]
 fn cli_tools() -> Html
 {
+    let cols = Rc::from(["Name", "Description", "Link"].map(AttrValue::from));
+
     html! {
-        {"CLI Tools I like"}
+        <div class="flex flex-col text-primary">
+
+            <Table caption="CLI Tools I like" {cols}/>
+
+        </div>
     }
 }
