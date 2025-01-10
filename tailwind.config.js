7aa8404bf150d4aba0202f351b0e2688496a6f13
Callum Adair <callum.adair@outlook.com>
2025-01-10 12:50:18 +0000

    initial custom theming

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,47 @@ module.exports = {
     ],
 
     daisyui: {
-        themes: true,
+        themes: [
+            {
+                custom: {
+                    ...require("daisyui/src/theming/themes")["dark"],
+                    "primary": "red"
+                }
+            },
+
+            "light",
+            "dark",
+            "cupcake",
+            "bumblebee",
+            "emerald",
+            "corporate",
+            "synthwave",
+            "retro",
+            "cyberpunk",
+            "valentine",
+            "halloween",
+            "garden",
+            "forest",
+            "aqua",
+            "lofi",
+            "pastel",
+            "fantasy",
+            "wireframe",
+            "black",
+            "luxury",
+            "dracula",
+            "cmyk",
+            "autumn",
+            "business",
+            "acid",
+            "lemonade",
+            "night",
+            "coffee",
+            "winter",
+            "dim",
+            "nord",
+            "sunset",
+        ],
         darkTheme: "dark",
         base: true,
         styled: true,
