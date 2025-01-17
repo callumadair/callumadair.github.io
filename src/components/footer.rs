99120caa63daee89985e609f9cc3f89f6877f3e8
Callum Adair <callum.adair@outlook.com>
2025-01-17 22:36:33 +0000

    initial footer impl

---
+++ src/components/footer.rs
@@ -0,0 +1,22 @@
+use chrono::{
+    Datelike,
+    Utc,
+};
+use yew::prelude::*;
+
+#[function_component(Footer)]
+pub fn footer() -> Html
+{
+    let cur_year = Utc::now().year();
+    html! {
+        <footer class="p-4 footer footer-center bg-base-300 text-base-content">
+
+            <aside>
+                <p>
+                    {format!("Copyright © 2024 - {cur_year}. All rights reserved by Callum Adair." )}
+                </p>
+            </aside>
+
+        </footer>
+    }
+}
