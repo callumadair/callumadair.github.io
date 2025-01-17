59b34e89739ab31a7a79d3a764881ad969ec6a9d
Callum Adair <callum.adair@outlook.com>
2025-01-17 22:10:52 +0000

    added from impl

--- src/pages/software.rs
+++ src/pages/software.rs
@@ -1,5 +1,6 @@
 use std::rc::Rc;
 
+use shared::software::SoftwareTool;
 use yew::prelude::*;
 
 use crate::{
@@ -14,15 +15,30 @@ use crate::{
 };
 
 #[derive(PartialEq, Clone)]
-pub(crate) struct SoftwareTool
+pub(crate) struct SoftwareToolRow
 {
     pub(crate) name:       String,
     pub(crate) short_desc: String,
-    pub(crate) link:       String,
     pub(crate) long_desc:  String,
+    pub(crate) web_link:   String,
+    pub(crate) image_link: String,
 }
 
-impl ToHtml for SoftwareTool
+impl From<SoftwareTool> for SoftwareToolRow
+{
+    fn from(value: SoftwareTool) -> Self
+    {
+        Self {
+            name:       value.name,
+            short_desc: value.short_desc,
+            long_desc:  value.long_desc,
+            web_link:   value.web_link,
+            image_link: value.image_link,
+        }
+    }
+}
+
+impl ToHtml for SoftwareToolRow
 {
     fn to_html(&self) -> Html
     {
@@ -38,7 +54,7 @@ impl ToHtml for SoftwareTool
                 </td>
 
                 <td>
-                    <a target="_blank" href={self.link.clone()}>
+                    <a target="_blank" href={self.web_link.clone()}>
                         {"Website"}
                     </a>
                 </td>
@@ -63,14 +79,17 @@ impl ToHtml for SoftwareTool
     }
 }
 
-impl Contains for SoftwareTool
+impl Contains for SoftwareToolRow
 {
     fn contains(
         &self,
         key: &str,
     ) -> bool
     {
-        self.short_desc.contains(key) || self.link.contains(key) || self.name.contains(key)
+        self.short_desc.contains(key)
+            || self.long_desc.contains(key)
+            || self.web_link.contains(key)
+            || self.name.contains(key)
     }
 }
 
@@ -94,7 +113,7 @@ fn cli_tools() -> Html
     html! {
         <div class="flex flex-col w-full text-primary">
 
-            <Table<SoftwareTool>
+            <Table<SoftwareToolRow>
                 id="cli-table"
                 caption="CLI Tools I like"
                 {cols}
@@ -108,25 +127,28 @@ fn cli_tools() -> Html
 
 // TODO replace this with an HTTP GET to the backend
 // whenever I get round to making it if ever.
-fn get_rows() -> Vec<SoftwareTool>
+fn get_rows() -> Vec<SoftwareToolRow>
 {
-    let starship = SoftwareTool {
+    let starship = SoftwareToolRow {
         name:       "Starship".to_string(),
         short_desc: "A nice modern terminal prompt".to_string(),
-        link:       "https://starship.rs".to_string(),
+        web_link:   "https://starship.rs".to_string(),
         long_desc:  String::new(),
+        image_link: String::new(),
     };
-    let hyperfine = SoftwareTool {
+    let hyperfine = SoftwareToolRow {
         name:       "Hyperfine".to_string(),
         short_desc: "A benchmarking tool written in rust".to_string(),
-        link:       "https://github.com/sharkdp/hyperfine".to_string(),
+        web_link:   "https://github.com/sharkdp/hyperfine".to_string(),
         long_desc:  String::new(),
+        image_link: String::new(),
     };
-    let nushell = SoftwareTool {
+    let nushell = SoftwareToolRow {
         name:       "Nushell".to_string(),
         short_desc: "A new way of doing shells".to_string(),
-        link:       "https://www.nushell.sh".to_string(),
+        web_link:   "https://www.nushell.sh".to_string(),
         long_desc:  String::new(),
+        image_link: String::new(),
     };
 
     vec![starship, hyperfine, nushell]
