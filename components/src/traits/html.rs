2001ff5ac39d50f1bc61cd316ce892db133278bb
Callum Adair <callum.adair@outlook.com>
2025-01-25 15:06:30 +0000

    bring back modal display for software tool

--- components/src/traits/html.rs
+++ components/src/traits/html.rs
@@ -1,5 +1,7 @@
 use shared::software::SoftwareTool;
 use yew::prelude::*;
+use crate::traits::modal::ModalDisplay;
+
 pub trait ToHtmlWrapper
 {
     fn html(&self) -> Html;
@@ -33,7 +35,7 @@ impl ToHtmlWrapper for SoftwareTool
                 </td>
 
                 <td>
-                    // {self.display()}
+                    {self.display()}
                 </td>
 
 
