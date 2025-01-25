ab1015f6472fa1109fb1c6456c4c486e162583c2
Callum Adair <callum.adair@outlook.com>
2025-01-25 14:41:09 +0000

    removed daisyui from tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,6 @@ module.exports = {
         extend: {},
     },
     plugins: [
-        require('daisyui'),
         require('@tailwindcss/forms')({
             strategy: 'base',
             strategy: 'class',
