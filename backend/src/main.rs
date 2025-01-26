b744e4fde6a60cf8d4bc817b90579d94c701fe82
Callum Adair <callum.adair@outlook.com>
2025-01-26 22:01:53 +0000

    initial main setup

---
+++ backend/src/main.rs
@@ -0,0 +1,81 @@
+use actix_settings::{
+    ApplySettings as _,
+    Mode,
+    Settings,
+};
+use actix_web::{
+    middleware::{
+        Compress,
+        Condition,
+    },
+    web::Data,
+    App,
+    HttpServer,
+};
+use portfolio_backend_lib::http_api::handlers::index::index;
+use tracing_actix_web::TracingLogger;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()>
+{
+    let mut settings =
+        Settings::parse_toml("./Server.toml").expect("Failed to parse `Settings` from Server.toml");
+
+    // If the environment variable `$APPLICATION__HOSTS` is set,
+    // have its value override the `settings.actix.hosts`
+    // setting:
+    Settings::override_field_with_env_var(&mut settings.actix.hosts, "APPLICATION__HOSTS")?;
+
+    init_logger(&settings);
+
+    HttpServer::new({
+        // clone settings into each worker thread
+        let settings = settings.clone();
+
+        move || {
+            App::new()
+                // Include this `.wrap()` call for compression settings to take effect
+                .wrap(Condition::new(
+                    settings.actix.enable_compression,
+                    Compress::default(),
+                ))
+
+                // add request logger
+                .wrap(TracingLogger::default())
+
+                // make `Settings` available to handlers
+                .app_data(Data::new(settings.clone()))
+
+                // add request handlers as normal
+                .service(index)
+        }
+    })
+    // apply the `Settings` to Actix Web's `HttpServer`
+    .try_apply_settings(&settings)?
+    .run()
+    .await
+}
+
+/// Initialize the logging infrastructure.
+fn init_logger(settings: &Settings)
+{
+    if !settings.actix.enable_log
+    {
+        return;
+    }
+
+    unsafe {
+        std::env::set_var(
+            "RUST_LOG",
+            match settings.actix.mode
+            {
+                Mode::Development => "actix_web=debug",
+                Mode::Production => "actix_web=info",
+            },
+        );
+
+        std::env::set_var("RUST_BACKTRACE", "1");
+    }
+
+    tracing_subscriber::fmt::init();
+}
