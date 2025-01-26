25c13b2c2284910e39919951970e10ee6f30d880
Callum Adair <callum.adair@outlook.com>
2025-01-26 22:08:26 +0000

    added example postgres connection

--- backend/src/database/mod.rs
+++ backend/src/database/mod.rs
@@ -1,11 +1,11 @@
 // use std::time::Duration;
 // use sea_orm::sqlx::PgConnection;
 // use sea_orm::sqlx::postgres::PgConnectOptions;
-// 
+//
 // const DATABASE_URL: &str = "";
 // fn init_connection() -> PgConnection {
 //     let mut opt = PgConnectOptions::new(DATABASE_URL);
-//     
+//
 //     opt.max_connections(100)
 //     .min_connections(5)
 //     .connect_timeout(Duration::from_secs(8))
@@ -14,5 +14,5 @@
 //     .max_lifetime(Duration::from_secs(8))
 //     .sqlx_logging(true)
 //     .sqlx_logging_level(log::LevelFilter::Info)
-//     .set_schema_search_path("my_schema"); 
+//     .set_schema_search_path("my_schema");
 // }
