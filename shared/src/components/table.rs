cb5defd1c5c7e908f3a79a3c153c1d2d79fdee67
Callum Adair <callum.adair@outlook.com>
2025-02-15 18:47:59 +0000

    better colours

--- shared/src/components/table.rs
+++ shared/src/components/table.rs
@@ -51,7 +51,7 @@ pub fn base<T: PartialEq + ToHtml + Clone + Contains + 'static>(props: &TablePro
     };
 
     html! {
-        <div class="grid grid-cols-1 gap-5 p-3 overflow-auto rounded-lg rounded-box border">
+        <div class="grid grid-cols-1 gap-5 p-3 overflow-auto rounded-lg rounded-box text-info bg-info-content">
 
             <h2 class="flex w-full p-3 items-center justify-between text-left text-2xl">
                 { title.to_string() }
