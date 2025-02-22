91ee64523ef7a54b980fe93b20d450373db9c671
Callum Adair <callum.adair@outlook.com>
2025-02-22 12:41:46 +0000

    added initial better props for button.rs

--- shared/src/components/button.rs
+++ shared/src/components/button.rs
@@ -1,28 +1,29 @@
-use strum::AsRefStr;
+use strum::{
+    AsRefStr,
+    Display,
+};
 use yew::prelude::*;
-
-use crate::components::Colour;
 
 crate::component_colours!(ButtonColour, "btn-");
 
-#[derive(Clone, Copy, Debug, Eq, Default, PartialEq, AsRefStr)]
-#[strum(prefix = "btn", serialize_all = "kebab-case")]
+#[derive(Clone, Copy, Debug, Eq, Default, Display, PartialEq, AsRefStr)]
+#[strum(prefix = "btn-", serialize_all = "kebab-case")]
 pub enum Behaviour
 {
-    #[default]
     Active,
     Disabled,
+    #[default]
+    None,
 }
 
-#[derive(Clone, Copy, Debug, Eq, Default, PartialEq, AsRefStr)]
-#[strum(prefix = "btn", serialize_all = "kebab-case")]
-pub enum Style
+#[derive(Clone, Copy, Debug, Eq, Default, Display, PartialEq, AsRefStr)]
+#[strum(prefix = "btn-", serialize_all = "kebab-case")]
+pub enum ButtonStyle
 {
     Outline,
     Soft,
     Ghost,
     Link,
-    #[strum(serialize = "")]
     #[default]
     None,
 }
@@ -30,18 +31,34 @@ pub enum Style
 #[derive(Properties, PartialEq, Clone)]
 pub struct ButtonProps
 {
+    #[prop_or_default]
     pub colour:   ButtonColour,
+    #[prop_or_default]
+    pub style:    ButtonStyle,
     pub children: Children,
 }
 
+#[derive(Clone, Copy, Debug, Default, PartialEq, Eq, derive_more::Display)]
+#[display("btn {colour} {style}")]
+struct ButtonClasses
+{
+    colour: ButtonColour,
+    style:  ButtonStyle,
+}
 #[function_component(Button)]
 pub fn button(props: &ButtonProps) -> Html
 {
-    let ButtonProps { colour, children } = props.clone();
-    let class = colour.as_ref().to_string();
-    let colour_string = ButtonColour::Accent.as_ref();
-    gloo::console::log!(colour_string);
-
+    let ButtonProps {
+        colour,
+        style,
+        children,
+    } = props.clone();
+    let class = ButtonClasses {
+        colour: ButtonColour::Accent,
+        style,
+    }
+    .to_string();
+    gloo::console::log!(&class);
     html! {
         <button {class}>
             {children}
