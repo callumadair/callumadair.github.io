5658c0e77422214399d4af65c2bacbb060ad37a2
Callum Adair <callum.adair@outlook.com>
2025-02-23 16:13:09 +0000

    delete sample migration

--- backend/migration/src/lib.rs
+++ backend/migration/src/lib.rs
@@ -6,8 +6,10 @@ mod m20250216_153522_image_links;
 pub struct Migrator;
 
 #[async_trait::async_trait]
-impl MigratorTrait for Migrator {
-    fn migrations() -> Vec<Box<dyn MigrationTrait>> {
+impl MigratorTrait for Migrator
+{
+    fn migrations() -> Vec<Box<dyn MigrationTrait>>
+    {
         vec![
             Box::new(m20250216_151738_software_tools::Migration),
             Box::new(m20250216_153522_image_links::Migration),
