0f3726fceab6f4f62c958043afd362d84e3c149e
Callum Adair <callum.adair@outlook.com>
2025-02-28 17:40:29 +0000

    convert footer to dioxus

--- shared/src/components/footer.rs
+++ shared/src/components/footer.rs
@@ -2,21 +2,21 @@ use chrono::{
     Datelike,
     Utc,
 };
-use yew::prelude::*;
+use dioxus::prelude::*;
 
-#[function_component(Footer)]
-pub fn footer() -> Html
+#[component]
+pub fn footer() -> Element
 {
     let cur_year = Utc::now().year();
-    html! {
-        <footer class="footer footer-center bg-base-300 text-base-content p-4">
+    rsx! {
+        footer {
+            class: "footer footer-center bg-base-300 text-base-content p-4",
+            aside {
+                    p {
+                        {format!("Copyright © 2024 - {cur_year}. All rights reserved by Callum Adair." )}
+                    }
+            }
 
-            <aside>
-                <p>
-                    {format!("Copyright © 2024 - {cur_year}. All rights reserved by Callum Adair." )}
-                </p>
-            </aside>
-
-        </footer>
+        }
     }
 }
