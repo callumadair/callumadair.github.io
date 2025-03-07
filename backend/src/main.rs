b267a18277211ea1f414d74090aeca24dbfd50d8
Callum Adair <callum.adair@outlook.com>
2025-03-07 23:30:52 +0000

    use AppState and instantiate database connection

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,24 +1,31 @@
 use actix_settings::{
-    ApplySettings as _,
+    ApplySettings,
     Mode,
     Settings,
 };
 use actix_web::{
+    App,
+    HttpServer,
     middleware::{
         Compress,
         Condition,
     },
     web::Data,
-    App,
-    HttpServer,
 };
-use portfolio_backend_lib::http_api::handlers::index::index;
+use portfolio_backend_lib::{
+    AppState,
+    http_api::handlers::index::index,
+};
+use sea_orm::{
+    Database,
+    DatabaseConnection,
+};
 use tracing_actix_web::TracingLogger;
 use utoipa_actix_web::AppExt;
 use utoipa_swagger_ui::SwaggerUi;
 
 #[tokio::main]
-async fn main() -> std::io::Result<()>
+async fn main() -> color_eyre::Result<()>
 {
     let mut settings =
         Settings::parse_toml("./Server.toml").expect("Failed to parse `Settings` from Server.toml");
@@ -27,23 +34,31 @@ async fn main() -> std::io::Result<()>
     // have its value override the `settings.actix.hosts`
     // setting:
     Settings::override_field_with_env_var(&mut settings.actix.hosts, "APPLICATION__HOSTS")?;
+    let db_conn: DatabaseConnection =
+        Database::connect("postgres://postgres:password@localhost/database").await?;
 
     init_logger(&settings);
 
-    HttpServer::new( {
+    HttpServer::new({
         // clone settings into each worker thread
         let settings = settings.clone();
+        let db_conn = db_conn.clone();
 
         move || {
             App::new()
                 .into_utoipa_app()
                 .map(|app|
                     app.wrap(Condition::new(
-                       settings.actix.enable_compression,
-                       Compress::default(),
-                   ))
-                   .app_data(Data::new(settings.clone()))
-                   .wrap(TracingLogger::default())
+                        settings.actix.enable_compression,
+                        Compress::default(),
+                    ))
+                        .app_data(Data::new(
+                            AppState {
+                                db_conn: db_conn.clone(),
+                                settings: settings.clone(),
+                            }
+                        ))
+                        .wrap(TracingLogger::default())
                 )
                 .service(index)
                 .openapi_service(|api| {
@@ -55,7 +70,9 @@ async fn main() -> std::io::Result<()>
         // apply the `Settings` to Actix Web's `HttpServer` 
         .try_apply_settings(&settings)?
         .run()
-        .await
+        .await?;
+
+    Ok(())
 }
 
 /// Initialize the logging infrastructure.
