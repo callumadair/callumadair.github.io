c0134bdedab43b8e717455d9c2282066e7b6d4aa
Callum Adair <callum.adair@outlook.com>
2025-03-07 23:29:43 +0000

    moved AppState

--- backend/src/http_api/handlers/mod.rs
+++ backend/src/http_api/handlers/mod.rs
@@ -1,9 +1,1 @@
-use sea_orm::DatabaseConnection;
-
 pub mod index;
-
-#[derive(Debug, Clone)]
-struct AppState
-{
-    conn: DatabaseConnection,
-}
--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,3 +1,16 @@
+use actix_settings::{
+    BasicSettings,
+    NoSettings,
+};
+use sea_orm::DatabaseConnection;
+
 pub mod database;
 pub mod error;
 pub mod http_api;
+
+#[derive(Debug, Clone)]
+pub struct AppState
+{
+    pub db_conn:  DatabaseConnection,
+    pub settings: BasicSettings<NoSettings>,
+}
