a3dd080f019acd21ad271d246c8ddbde5814c1ea
Callum Adair <callum.adair@outlook.com>
2025-03-07 18:12:42 +0000

    working build script for tailwind

--- frontend/build.rs
+++ frontend/build.rs
@@ -1,20 +1,38 @@
-fn main()
+//! Build script based off the example [found here](https://github.com/LyonSyonII/dioxus-tailwindcss/blob/main/build.rs)
+
+use strum::{
+    AsRefStr,
+    IntoStaticStr,
+};
+
+/// Checks package runner is installed and runs tailwind.
+pub fn main()
 {
     let toolchain = install_packages();
+
     // Compile TailwindCSS .css file
     std::process::Command::new(toolchain)
-        .args([
-            "@tailwind/cli",
-            "-i",
-            "./main.css",
-            "-o",
-            "./assets/out.css",
-            "--minify",
-        ])
-        .env("NODE_ENV", "production")
-        .spawn()
+        .args(["tailwindcss", "-i", "./main.css", "-o", "./assets/out.css"])
+        .output()
         .expect("Failed to run tailwind.");
 }
+
+#[derive(AsRefStr)]
+#[strum(serialize_all = "lowercase")]
+enum PackageManagers
+{
+    Bun,
+    Npm,
+}
+
+#[derive(IntoStaticStr)]
+#[strum(serialize_all = "lowercase")]
+enum BuildTools
+{
+    BunX,
+    NpX,
+}
+
 /// Installs required packages and selects toolchain to use.
 ///
 ///
@@ -22,14 +40,21 @@ fn main()
 /// Will panic if none of the toolchains is installed.
 fn install_packages() -> &'static str
 {
-    let bun = "bunx";
-
-    if std::process::Command::new(bun)
+    if std::process::Command::new(PackageManagers::Bun.as_ref())
         .arg("install")
         .spawn()
         .is_ok()
     {
-        return bun;
+        return BuildTools::BunX.into();
     }
-    panic!("Bunx is not installed");
+
+    if std::process::Command::new(PackageManagers::Npm.as_ref())
+        .arg("install")
+        .spawn()
+        .is_ok()
+    {
+        return BuildTools::NpX.into();
+    }
+
+    panic!("Bun is not installed");
 }
