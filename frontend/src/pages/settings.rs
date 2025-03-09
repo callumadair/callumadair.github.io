120b3adb354e78b7a01045798d1124b62c1460c4
Callum Adair <callum.adair@outlook.com>
2025-03-09 14:57:54 +0000

    added demo box for code in settings.rs

--- frontend/src/pages/settings.rs
+++ frontend/src/pages/settings.rs
@@ -5,16 +5,24 @@ pub fn SettingsBase() -> Element
 {
     rsx! {
         div {
-            class: "flex items-start justify-start p-5",
+            class: "flex flex-col gap-5 items-start justify-start p-5",
 
             h2 {
                 class: "text-xl",
                 "Settings"
             }
 
-            h3 {
-                class: "text-lg",
-                "Themes"
+            div {
+                class: "flex flex-col gap-3",
+                h3 {
+                    class: "text-lg",
+                    "Themes"
+                }
+
+                code {
+                    class: "p-3 rounded-box bg-base-300",
+                    "Hello, world!"
+                }
             }
         }
     }
