2aa46d4c8a598d55e6fa39d5a7905142b31fb687
Callum Adair <callum.adair@outlook.com>
2025-03-16 23:54:09 +0000

    initial impl of accordion

---
+++ shared/src/components/data_display/accordion.rs
@@ -0,0 +1,96 @@
+use dioxus::prelude::*;
+
+use crate::components::{
+    collapse::CollapseModifier,
+    BackgroundColour,
+    PeerBackgroundColour,
+    PeerTextColour,
+    TextColour,
+};
+
+#[derive(Clone, Copy, Default, PartialEq, Eq, derive_more::Display)]
+#[display("collapse bg-base-100 border border-base-300 {open_state} {symbol}")]
+struct AccordionClasses
+{
+    open_state: CollapseModifier,
+    symbol:     CollapseModifier,
+}
+
+impl From<AccordionItemProps> for AccordionClasses
+{
+    fn from(value: AccordionItemProps) -> Self
+    {
+        Self {
+            open_state: value.open_state.unwrap_or_default(),
+            symbol:     value.symbol.unwrap_or_default(),
+        }
+    }
+}
+
+#[derive(Props, PartialEq, Clone)]
+struct AccordionItemProps
+{
+    accordion_name:            String,
+    checked_background_colour: Option<PeerBackgroundColour>,
+    checked_text_colour:       Option<PeerTextColour>,
+    children:                  Element,
+    initial_background_colour: Option<BackgroundColour>,
+    initial_text_colour:       Option<TextColour>,
+    open_state:                Option<CollapseModifier>,
+    symbol:                    Option<CollapseModifier>,
+    title:                     String,
+}
+
+#[component]
+pub fn AccordionItem(props: AccordionItemProps) -> Element
+{
+    let class = AccordionClasses::from(props.clone()).to_string();
+
+    let AccordionItemProps {
+        accordion_name,
+        checked_background_colour,
+        checked_text_colour,
+        initial_background_colour,
+        initial_text_colour,
+        children,
+        title,
+        ..
+    } = props;
+
+    let title_class = format!(
+        "collapse-title {} {} peer-checked:{} peer-checked:{}",
+        initial_background_colour.unwrap_or_default(),
+        initial_text_colour.unwrap_or_default(),
+        checked_background_colour.unwrap_or_default(),
+        checked_text_colour.unwrap_or_default(),
+    );
+
+    let content_class = format!(
+        "collapse-content {} {} peer-checked:{} peer-checked:{}",
+        initial_background_colour.unwrap_or_default(),
+        initial_text_colour.unwrap_or_default(),
+        checked_background_colour.unwrap_or_default(),
+        checked_text_colour.unwrap_or_default(),
+    );
+
+    rsx! {
+        div {
+            class,
+
+            input {
+                type: "radio",
+                name: accordion_name,
+            }
+
+            div {
+                class: title_class,
+                {title}
+            }
+
+            div {
+                class: content_class,
+                {children}
+            }
+        }
+    }
+}
