78d24d85e6a97e57f0c6350dff9c3c743e0c9b41
Callum Adair <callum.adair@outlook.com>
2025-07-27 15:42:06 +0100

    address clippy warning

--- backend/entity/src/software.rs
+++ backend/entity/src/software.rs
@@ -29,7 +29,7 @@ impl Responder for Model
 
     fn respond_to(
         self,
-        req: &HttpRequest,
+        _req: &HttpRequest,
     ) -> HttpResponse<Self::Body>
     {
         let body = serde_json::to_string(&self).unwrap();
