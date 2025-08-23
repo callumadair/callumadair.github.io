77b42b8595467c98c55953c3721ced01572808aa
Callum Adair <callum.adair@outlook.com>
2025-08-23 15:11:52 +0100

    refactor(Error Handling): :recycle: improvements to the structure of InstantiationError types

--- backend/entity/src/error.rs
+++ backend/entity/src/error.rs
@@ -1,5 +1,8 @@
 use actix_web::http::StatusCode;
-use shared::impl_nested_status_code;
+use shared::{
+    impl_nested_error,
+    impl_nested_status_code,
+};
 
 #[derive(Debug, thiserror::Error)]
 pub enum EntityError
@@ -8,12 +11,48 @@ pub enum EntityError
     InstantiationError(#[from] InstantiationError),
 }
 impl_nested_status_code!(EntityError, InstantiationError);
+impl_nested_error!(
+    EntityError,
+    InstantiationError,
+    ImageInstantiationError,
+    SoftwareInstantiationError
+);
 
 #[derive(Debug, thiserror::Error)]
 pub enum InstantiationError
 {
+    #[error("{0}")]
+    ImageInstantiationError(#[from] ImageInstantiationError),
+    #[error("{0}")]
+    SoftwareInstantiationError(#[from] SoftwareInstantiationError),
+}
+impl_nested_status_code!(
+    InstantiationError,
+    ImageInstantiationError,
+    SoftwareInstantiationError
+);
+
+#[derive(Debug, thiserror::Error)]
+pub enum ImageInstantiationError
+{
     #[error("Image URL cannot be empty")]
     ImageURLEmpty,
+}
+
+impl actix_web::ResponseError for ImageInstantiationError
+{
+    fn status_code(&self) -> StatusCode
+    {
+        match self
+        {
+            Self::ImageURLEmpty => StatusCode::BAD_REQUEST,
+        }
+    }
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum SoftwareInstantiationError
+{
     #[error("Software name cannot be empty")]
     SoftwareNameEmpty,
     #[error("Software short description cannot be empty")]
@@ -24,14 +63,13 @@ pub enum InstantiationError
     SoftwareWebLinkEmpty,
 }
 
-impl actix_web::ResponseError for InstantiationError
+impl actix_web::ResponseError for SoftwareInstantiationError
 {
-    fn status_code(&self) -> actix_web::http::StatusCode
+    fn status_code(&self) -> StatusCode
     {
         match self
         {
-            Self::ImageURLEmpty
-            | Self::SoftwareNameEmpty
+            Self::SoftwareNameEmpty
             | Self::SoftwareShortDescriptionEmpty
             | Self::SoftwareLongDescriptionEmpty
             | Self::SoftwareWebLinkEmpty => StatusCode::BAD_REQUEST,
