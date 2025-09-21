9f5efedc802909e8a3d148498e66744b71e588c5
Callum Adair <callum.adair@outlook.com>
2025-09-21 18:37:52 +0100

    add primevue to nuxt frontend

--- nuxt-frontend/nuxt.config.ts
+++ nuxt-frontend/nuxt.config.ts
@@ -1,15 +1,20 @@
 import tailwindcss from "@tailwindcss/vite";
+import Aura from "@primevue/themes/aura";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	compatibilityDate: "2025-07-15",
 	devtools: { enabled: true },
-	modules: ["@nuxt/test-utils", "@nuxt/ui"],
+	modules: ["@nuxt/test-utils", "@primevue/nuxt-module"],
 	css: ["~/assets/css/main.css"],
 	vite: {
 		plugins: [tailwindcss()],
 	},
-	ui: {
-		fonts: true,
+	primevue: {
+		options: {
+			theme: {
+				preset: Aura,
+			},
+		},
 	},
 });
