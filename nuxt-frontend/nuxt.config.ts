4edb1dbeb86df8691757283bc0b65fa695e19e20
Callum Adair <callum.adair@outlook.com>
2025-11-07 00:32:18 +0000

    devtools improvements

--- nuxt-frontend/nuxt.config.ts
+++ nuxt-frontend/nuxt.config.ts
@@ -3,7 +3,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	compatibilityDate: "2025-07-15",
-	devtools: { enabled: true },
 	modules: [
 		"@nuxt/test-utils",
 		"@nuxt/ui",
@@ -13,4 +12,12 @@ export default defineNuxtConfig({
 	vite: {
 		plugins: [tailwindcss()],
 	},
+	devtools: {
+		enabled: true,
+		vscode: {
+			codeServer: "ms-code-server",
+			host: "0.0.0.0",
+			port: 3090
+		}
+	}
 });
