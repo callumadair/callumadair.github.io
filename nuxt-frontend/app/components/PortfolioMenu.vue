f87cf00d37c92a42309fa0df3c4e68c1b693d504
Callum Adair <callum.adair@outlook.com>
2025-11-11 16:42:06 +0000

    tidying up

--- nuxt-frontend/app/components/PortfolioMenu.vue
+++ nuxt-frontend/app/components/PortfolioMenu.vue
@@ -62,29 +62,44 @@ const textSize = "text-sm";
 </script>
 
 <template>
-    <aside class="p-2 h-svh flex flex-col items-center border-r border-grey-accent-400">
-        <div class="flex flex-row w-full">
-            <button @click="toggleCollapse"
-                class="p-2 flex grow justify-start items-center bg-ui hover:bg-muted hover:cursor-pointer space-x-2.5">
-                <Icon :name="collapseButtonIcon" :size="iconSize" />
-                <span v-if="!collapsed" :class="textSize">
-                    Collapse
-                </span>
-            </button>
-        </div>
-        <div class="flex flex-col grow justify-between">
-            <div v-for="section in items">
-                <NuxtLink v-for="item in section" :class="item.class"
-                    class="p-2 hover:bg-muted flex flex-row items-center space-x-2.5" @click="item.onSelect"
-                    :to="item.to" :target="item.target">
-                    <Icon v-if="item.icon && (typeof item.icon) === 'string'" :name="item.icon" :size="iconSize" />
-                    <span v-if="!collapsed && item.label && (typeof item.label) === 'string'" class="text-nowrap"
-                        :class="textSize">
-                        {{ item.label }}
+    <TransitionGroup>
+        <aside class="p-2 h-svh flex flex-col center border-r border-grey-accent-500 items-start"
+            :class="!collapsed ? 'w-56' : ''">
+            <div class="flex flex-row w-full">
+                <button @click="toggleCollapse"
+                    class="p-2 flex grow justify-start items-center bg-ui hover:bg-muted hover:cursor-pointer space-x-2.5">
+                    <Icon :name="collapseButtonIcon" :size="iconSize" />
+                    <span v-if="!collapsed" :class="textSize">
+                        Collapse
                     </span>
-                </NuxtLink>
+                </button>
             </div>
-        </div>
+            <div class="flex flex-col grow justify-between w-full">
+                <div v-for="section in items">
+                    <NuxtLink v-for="item in section" :class="item.class"
+                        class="p-2 hover:bg-muted flex grow items-center space-x-2.5" @click="item.onSelect"
+                        :to="item.to" :target="item.target">
+                        <Icon v-if="item.icon && (typeof item.icon) === 'string'" :name="item.icon" :size="iconSize" />
+                        <span v-if="!collapsed && item.label && (typeof item.label) === 'string'" class="text-nowrap"
+                            :class="textSize">
+                            {{ item.label }}
+                        </span>
+                    </NuxtLink>
+                </div>
+            </div>
 
-    </aside>
+        </aside>
+    </TransitionGroup>
 </template>
+
+<style>
+.v-enter-active,
+.v-leave-active {
+    transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+    opacity: 0;
+}
+</style>
